e3c92066b90862bc1d663377c8f3acf6ecff47de
funnyjack <554517318@qq.com>
2023-10-30 17:09:29 +0800

    加上初始化 sql 以及阿里云的 mysql 连接方式

---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,21 @@
+create
+database if not exists test;
+use
+test;
+create table if not exists test
+(
+    name varchar
+(
+    128
+) not null,
+    message varchar
+(
+    128
+) not null,
+    primary key
+(
+    name
+)
+    );
+
+insert into test value ("鹏力","你好，我叫鹏力");
