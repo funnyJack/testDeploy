9d7dfef63d9eb870c5b16388b3a647a1b3c81b9b
funnyjack <554517318@qq.com>
2024-04-08 15:31:25 +0800

    使用 testRestTemplate 测试 MVC 接口

--- monolith-service/src/test/kotlin/com/funnyjack/testdeploy/TestDeployApplicationTests.kt
+++ monolith-service/src/test/kotlin/com/funnyjack/testdeploy/TestDeployApplicationTests.kt
@@ -5,21 +5,27 @@ import com.funnyjack.testdeploy.model.TestCreationModel
 import com.funnyjack.testdeploy.model.TestViewModel
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
 
 import kotlin.test.assertEquals
 
-
 class TestDeployApplicationTests @Autowired constructor(
-    private val webTestClient: WebTestClient,
+    private val restTemplate: TestRestTemplate,
 ) : TestDeploySpringBootTest() {
+
     @Test
     fun create() {
-        val testViewModel = webTestClient.post().uri("/test")
-            .bodyValue(TestCreationModel(name = "test", message = "testCreate"))
-            .exchange().expectStatus().isOk
-            .expectBody(TestViewModel::class.java)
-            .returnResult().responseBody!!
+        val createUrl = "/test"
+        val createModel = HttpEntity(TestCreationModel(name = "test", message = "testCreate"))
+//      val testViewModel =  restTemplate.postForObject(createUrl,createModel,TestViewModel::class.java)!!
+        val testViewModel = restTemplate.exchange(
+            createUrl,
+            HttpMethod.POST,
+            createModel,
+            TestViewModel::class.java
+        ).body!!
         assertEquals(testViewModel.name, "test")
         assertEquals(testViewModel.message, "testCreate")
     }
--- spring-test-starter/src/main/kotlin/com/funnyjack/test/SpringTestStarterApplication.kt
+++ spring-test-starter/src/main/kotlin/com/funnyjack/test/SpringTestStarterApplication.kt
@@ -1,17 +1,13 @@
 package com.funnyjack.test
 
-import com.hkmci.web.bms2.test.initializer.MysqlContainerInitializer
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.TestMethodOrder
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 
 //junit test base class must be an abstract class, can't be an interface
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@SpringJUnitConfig(initializers = [MysqlContainerInitializer::class])
-@AutoConfigureWebTestClient
+//@SpringJUnitConfig(initializers = [MysqlContainerInitializer::class])
 @ActiveProfiles("local", "test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 abstract class TestDeploySpringBootTest
