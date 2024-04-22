6981d7fb626f1dc0fcd2f983c40451aecad4c220
funnyJack <554517318@qq.com>
2024-04-22 21:14:10 +0800

    删除 JPA 依赖，JVM 版本设置为 17

--- src/main/kotlin/com/funnyjack/testdeploy/entity/Test.kt
+++ src/main/kotlin/com/funnyjack/testdeploy/entity/Test.kt
@@ -1,23 +1,23 @@
 package com.funnyjack.testdeploy.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
+//import jakarta.persistence.Column
+//import jakarta.persistence.Entity
+//import jakarta.persistence.GeneratedValue
+//import jakarta.persistence.GenerationType
 import org.springframework.data.annotation.Id
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 
-@Entity
+//@Entity
 class Test(
     @Id
-    @jakarta.persistence.Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @jakarta.persistence.Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-    @Column(length = 128, nullable = false)
+//    @Column(length = 128, nullable = false)
     var name: String = "",
-    @Column(length = 128, nullable = false)
+//    @Column(length = 128, nullable = false)
     var message: String = ""
 )
 
--- src/main/kotlin/com/funnyjack/testdeploy/model/TestModel.kt
+++ src/main/kotlin/com/funnyjack/testdeploy/model/TestModel.kt
@@ -1,30 +1,31 @@
 package com.funnyjack.testdeploy.model
 
 import com.funnyjack.testdeploy.entity.Test
-import com.funnyjack.testdeploy.jpaspecificationdsl.combineSpecification
-import com.funnyjack.testdeploy.jpaspecificationdsl.distinct
-import com.funnyjack.testdeploy.jpaspecificationdsl.like
-import com.funnyjack.testdeploy.utils.SearchFilterCombineOperation
-import com.funnyjack.testdeploy.utils.fullTrim
-import org.springframework.data.jpa.domain.Specification
+
+//import com.funnyjack.testdeploy.jpaspecificationdsl.combineSpecification
+//import com.funnyjack.testdeploy.jpaspecificationdsl.distinct
+//import com.funnyjack.testdeploy.jpaspecificationdsl.like
+//import com.funnyjack.testdeploy.utils.SearchFilterCombineOperation
+//import com.funnyjack.testdeploy.utils.fullTrim
+//import org.springframework.data.jpa.domain.Specification
 
 //
-data class TestSearchFilter(
-    val nameLike: String? = null,
-    val messageLike: String? = null
-) {
-    fun toSpecification(searchFilterCombineOperation: SearchFilterCombineOperation): Specification<Test> {
-        val nameLike = nameLike?.fullTrim()?.ifBlank { null }
-        val messageLike = messageLike?.fullTrim()?.ifBlank { null }
-        return combineSpecification(
-            listOf(
-                nameLike?.let { Test::name.like("%$it%") },
-                messageLike?.let { Test::message.like("%$it%") },
-            ),
-            searchFilterCombineOperation.toOperation()
-        ).distinct()
-    }
-}
+//data class TestSearchFilter(
+//    val nameLike: String? = null,
+//    val messageLike: String? = null
+//) {
+//    fun toSpecification(searchFilterCombineOperation: SearchFilterCombineOperation): Specification<Test> {
+//        val nameLike = nameLike?.fullTrim()?.ifBlank { null }
+//        val messageLike = messageLike?.fullTrim()?.ifBlank { null }
+//        return combineSpecification(
+//            listOf(
+//                nameLike?.let { Test::name.like("%$it%") },
+//                messageLike?.let { Test::message.like("%$it%") },
+//            ),
+//            searchFilterCombineOperation.toOperation()
+//        ).distinct()
+//    }
+//}
 
 data class TestCreationModel(
     val name: String,
@@ -37,10 +38,11 @@ data class TestPatchModel(
 )
 
 data class TestViewModel(
+    val id: Long,
     val name: String,
     val message: String
 )
 
 fun Test.toViewModel() = TestViewModel(
-    name, message
+    id!!, name, message
 )
