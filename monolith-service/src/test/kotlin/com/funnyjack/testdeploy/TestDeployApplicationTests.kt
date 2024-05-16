988ecc6805627bebab56b0c197da8b98269d0aab
funnyJack <554517318@qq.com>
2024-05-16 22:28:59 +0800

    测试 test container 的最新写法

--- monolith-service/src/test/kotlin/com/funnyjack/testdeploy/TestDeployApplicationTests.kt
+++ monolith-service/src/test/kotlin/com/funnyjack/testdeploy/TestDeployApplicationTests.kt
@@ -1,19 +1,30 @@
 package com.funnyjack.testdeploy
 
-import com.funnyjack.test.TestDeploySpringBootTest
 import com.funnyjack.testdeploy.model.TestCreationModel
 import com.funnyjack.testdeploy.model.TestViewModel
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
-
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import kotlin.test.assertEquals
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class TestDeployApplicationTests @Autowired constructor(
     private val restTemplate: TestRestTemplate,
-) : TestDeploySpringBootTest() {
+) {
+
+    companion object {
+        @Container
+        @ServiceConnection
+        val mysqlContainer = MySQLContainer("mysql:8").withDatabaseName("testDeploy")
+    }
 
     @Test
     fun create() {
